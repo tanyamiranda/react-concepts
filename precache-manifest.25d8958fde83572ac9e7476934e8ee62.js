c2b121c0031ef7e30a8013ade5c31cc02d037353
tanya <60669157+tanyamiranda@users.noreply.github.com>
2020-03-04 12:04:00 -0500

    Updates

---
+++ precache-manifest.25d8958fde83572ac9e7476934e8ee62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0091a9f88ba1264a3f6183701195617e",
+    "url": "/sample-app/index.html"
+  },
+  {
+    "revision": "2a223acee6492fdae758",
+    "url": "/sample-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9a29a0024bbe7164880c",
+    "url": "/sample-app/static/js/2.2df2bf89.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/sample-app/static/js/2.2df2bf89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a223acee6492fdae758",
+    "url": "/sample-app/static/js/main.5c7c0775.chunk.js"
+  },
+  {
+    "revision": "35a1f8b7a6053ca0f63b",
+    "url": "/sample-app/static/js/runtime-main.ec3999e6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sample-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
